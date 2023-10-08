0f17263e266db28d27a0f49bb5b2de21689b1fc0
David Zhang <daqing@v8os.com>
2023-10-08 17:03:47 +0800

    Add `resp` package

---
+++ lib/resp/error.go
@@ -0,0 +1,18 @@
+package resp
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ErrorMsg(c *gin.Context, msg string) {
+	log.Println("Error:", msg)
+
+	c.JSON(200, gin.H{"ok": false, "error": msg})
+	c.Abort()
+}
+
+func Error(c *gin.Context, err error) {
+	ErrorMsg(c, err.Error())
+}
---
+++ lib/resp/ok.go
@@ -0,0 +1,13 @@
+package resp
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func OK(c *gin.Context, data any) {
+	log.Printf("OK: %+v\n", data)
+
+	c.JSON(200, gin.H{"ok": true, "data": data})
+}
