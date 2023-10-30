9cac34f6064f1b7bedd39c02e1fdbdb7afe160b7
David Zhang <daqing@mzevo.com>
2023-10-30 08:34:13 +0800

    No need to use importmap

--- app/javascripts/src/home_index.jsx
+++ app/javascripts/src/home_index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 function MyButton() {
   return (
-    <button>
+    <button className='bg-green-600 px-4 py-2 rounded-full text-white' onClick={() => { alert('hi') }}>
       I'm a button
     </button>
   );
@@ -12,7 +12,7 @@ function MyButton() {
 let App = function MyApp() {
   return (
     <div>
-      <h1>Welcome to my app</h1>
+      <h1 className='text-sky-700'>React works</h1>
       <MyButton />
     </div>
   );
