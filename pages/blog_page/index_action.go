0196a30a293405c60ded4728ab2bc0bea5d4aaf3
David Zhang <daqing@mzevo.com>
2023-11-06 16:55:26 +0800

    Show blog posts

--- pages/blog_page/index_action.go
+++ pages/blog_page/index_action.go
@@ -3,17 +3,25 @@ package blog_page
 import (
 	"time"
 
+	"github.com/daqing/airway/api/post_api"
 	"github.com/daqing/airway/lib/page_resp"
 	"github.com/daqing/airway/lib/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func IndexAction(c *gin.Context) {
+	posts, err := post_api.Posts("id DESC", 0, 50)
+	if err != nil {
+		page_resp.Error(c, err)
+		return
+	}
+
 	data := map[string]any{
 		"Title":        BlogTitle(),
 		"Tagline":      BlogTagline(),
 		"Year":         time.Now().Year(),
 		"BlogRootPath": utils.PathPrefix("blog"),
+		"Posts":        posts,
 	}
 
 	page_resp.Page(c, "blog", "index", data)
