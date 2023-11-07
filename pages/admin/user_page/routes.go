3ca8d3e36b92648c313101dcaf3a173923cf75ef
David Zhang <daqing@mzevo.com>
2023-11-07 14:05:29 +0800

    Create user in admin

--- pages/admin/user_page/routes.go
+++ pages/admin/user_page/routes.go
@@ -9,5 +9,7 @@ func Routes(r *gin.RouterGroup) {
 	g := r.Group("/user")
 	{
 		g.GET("", helper.CheckAdmin(IndexAction))
+		g.GET("/new", helper.CheckAdmin(NewAction))
+		g.POST("/create", helper.CheckAdmin(CreateAction))
 	}
 }
