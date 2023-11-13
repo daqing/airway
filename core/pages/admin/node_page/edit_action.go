0d4e92409b5abc1a4ea31c2a7e4a00aff0fb7235
David Zhang <daqing@mzevo.com>
2023-11-13 19:44:53 +0800

    Admin Edit Node

---
+++ core/pages/admin/node_page/edit_action.go
@@ -0,0 +1,30 @@
+package node_page
+
+import (
+	"github.com/daqing/airway/core/api/node_api"
+	"github.com/daqing/airway/lib/page_resp"
+	"github.com/daqing/airway/lib/repo"
+	"github.com/gin-gonic/gin"
+)
+
+func EditAction(c *gin.Context) {
+	id := c.Query("id")
+
+	node, err := repo.FindRow[node_api.Node](
+		[]string{"id", "name", "key"},
+		[]repo.KVPair{
+			repo.KV("id", id),
+		},
+	)
+
+	if err != nil {
+		page_resp.Error(c, err)
+		return
+	}
+
+	data := map[string]any{
+		"Node": node,
+	}
+
+	page_resp.Page(c, "core", "admin/node", "edit", data)
+}
--- core/pages/admin/node_page/routes.go
+++ core/pages/admin/node_page/routes.go
@@ -10,6 +10,9 @@ func Routes(r *gin.RouterGroup) {
 	{
 		g.GET("", helper.CheckAdmin(IndexAction))
 		g.GET("/new", helper.CheckAdmin(NewAction))
+		g.GET("/edit", helper.CheckAdmin(EditAction))
+
 		g.POST("/create", helper.CheckAdmin(CreateAction))
+		g.POST("/update", helper.CheckAdmin(UpdateAction))
 	}
 }
