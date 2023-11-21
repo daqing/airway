da81b2eee0c2546b8df4f5a0e0442b5f4af2795e
David Zhang <daqing@mzevo.com>
2023-11-21 19:55:39 +0800

    Update PathPrefix to omit trailing slash

--- lib/utils/base.go
+++ lib/utils/base.go
@@ -7,20 +7,19 @@ func GenerateApiToken() string {
 }
 
 const SLASH = "/"
-
-// const EMPTY_PATH = ""
+const EMPTY_PATH = ""
 
 // PathPrefix returns the path for an app,
 // regarding the env variable `AIRWAY_MULTI_APP`
 // if AIRWAY_MULTI_APP is "1", then call this function
-// with "blog" will return "/blog", otherwise returns
-// root path
+// with "blog" will return "/blog", otherwise it returns
+// empty path
 func PathPrefix(app string) string {
 	multiApp, _ := GetEnv("AIRWAY_MULTI_APP")
 
 	if multiApp == "1" {
-		return SLASH + app + SLASH
+		return SLASH + app
 	}
 
-	return SLASH
+	return EMPTY_PATH
 }
