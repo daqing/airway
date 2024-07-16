83b7d633c5b07adff6b7bd62025fafe7b0386b6a
David Zhang <daqing@mindgee.io>
2024-07-16 23:36:25 +0800

    Clean up routes

--- config/routes.go
+++ config/routes.go
@@ -2,20 +2,6 @@ package config
 
 import (
 	"github.com/daqing/airway/app/api/app_date_api"
-	"github.com/daqing/airway/core/api/checkin_api"
-	"github.com/daqing/airway/core/api/comment_api"
-	"github.com/daqing/airway/core/api/hello_api"
-	"github.com/daqing/airway/core/api/node_api"
-	"github.com/daqing/airway/core/api/post_api"
-	"github.com/daqing/airway/core/api/setting_api"
-	"github.com/daqing/airway/core/api/user_api"
-	"github.com/daqing/airway/core/pages/admin/admin_dashboard"
-	"github.com/daqing/airway/core/pages/admin/admin_menu"
-	"github.com/daqing/airway/core/pages/admin/admin_node"
-	"github.com/daqing/airway/core/pages/admin/admin_post"
-	"github.com/daqing/airway/core/pages/admin/admin_user"
-	"github.com/daqing/airway/core/pages/blog"
-	"github.com/daqing/airway/core/pages/forum"
 	"github.com/daqing/airway/core/pages/home_page"
 	"github.com/daqing/airway/core/pages/session_page"
 	"github.com/daqing/airway/core/pages/up_page"
@@ -25,51 +11,13 @@ import (
 func Routes(r *gin.Engine) {
 	r.GET("/", home_page.IndexAction)
 
-	coreRoutes(r)
-	appRoutes(r)
-
-	adminRoutes(r)
-}
-
-func adminRoutes(r *gin.Engine) {
-	g := r.Group("/admin")
-	{
-		admin_dashboard.Routes(g)
-
-		admin_user.Routes(g)
-		admin_post.Routes(g)
-		admin_node.Routes(g)
-		admin_menu.Routes(g)
-	}
-}
-
-func coreRoutes(r *gin.Engine) {
 	up_page.Routes(r)
 	session_page.Routes(r)
 
-	blog.Routes(r)
-	forum.Routes(r)
-
-	coreAPIRoutes(r)
+	apiRoutes(r)
 }
 
-func coreAPIRoutes(r *gin.Engine) {
-	v1 := r.Group("/api/v1")
-
-	checkin_api.Routes(v1)
-	comment_api.Routes(v1)
-	hello_api.Routes(v1)
-	node_api.Routes(v1)
-	post_api.Routes(v1)
-	setting_api.Routes(v1)
-	user_api.Routes(v1)
-}
-
-func appRoutes(r *gin.Engine) {
-	appAPIRoutes(r)
-}
-
-func appAPIRoutes(r *gin.Engine) {
+func apiRoutes(r *gin.Engine) {
 	v1 := r.Group("/api/v1")
 
 	app_date_api.Routes(v1)
