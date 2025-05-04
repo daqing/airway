d0ddf05f46fffc66e53eb773653aea142ad0ed24
David Zhang <daqing@mzevo.com>
2025-05-04 22:17:18 +0800

    Add OP cond

---
+++ lib/sql/op.go
@@ -0,0 +1,65 @@
+package sql
+
+func Eq(key string, val any) *Condition {
+	return &Condition{
+		Key: key,
+		Op:  "=",
+		Val: val,
+	}
+}
+
+func NotEq(key string, val any) *Condition {
+	return &Condition{
+		Key: key,
+		Op:  "<>",
+		Val: val,
+	}
+}
+
+func Gt(key string, val any) *Condition {
+	return &Condition{
+		Key: key,
+		Op:  ">",
+		Val: val,
+	}
+}
+
+func Gte(key string, val any) *Condition {
+	return &Condition{
+		Key: key,
+		Op:  ">=",
+		Val: val,
+	}
+}
+
+func Lt(key string, val any) *Condition {
+	return &Condition{
+		Key: key,
+		Op:  "<",
+		Val: val,
+	}
+}
+
+func Lte(key string, val any) *Condition {
+	return &Condition{
+		Key: key,
+		Op:  "<=",
+		Val: val,
+	}
+}
+
+func Like(key string, val any) *Condition {
+	return &Condition{
+		Key: key,
+		Op:  "LIKE",
+		Val: val,
+	}
+}
+
+func NotLike(key string, val any) *Condition {
+	return &Condition{
+		Key: key,
+		Op:  "NOT LIKE",
+		Val: val,
+	}
+}
