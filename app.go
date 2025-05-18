48a10573dcfff31f36ac6d92d7fca1b2215b0e08
David Zhang <daqing@mzevo.com>
2025-05-19 03:10:52 +0800

    Refactor code to app.go

---
+++ app.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/daqing/airway/config"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type App struct {
+	r    *gin.Engine
+	name string // Application name
+	port string
+}
+
+func NewApp(name, port string) *App {
+	router := gin.New()
+
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(CORS())
+
+	config.Routes(router)
+
+	return &App{
+		r:    router,
+		name: name,
+		port: port,
+	}
+}
+
+func (a *App) Router() *gin.Engine {
+	return a.r
+}
+
+func (a *App) Run() {
+	fmt.Printf("%s running at: http://127.0.0.1:%s\n", a.name, a.port)
+	a.r.Run(":" + a.port)
+}
+
+// Default CORS middleware
+func CORS() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		MaxAge:           12 * time.Hour,
+		AllowCredentials: true,
+	})
+}
--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"time"
 
-	"github.com/daqing/airway/config"
 	"github.com/daqing/airway/lib/repo/pg"
 	"github.com/daqing/airway/lib/utils"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -38,47 +34,6 @@ func main() {
 		pg.Setup(dsn)
 	}
 
-	port := utils.GetEnvMust("AIRWAY_PORT")
-	fmt.Printf("Airway running at: http://127.0.0.1:%s\n", port)
-
-	app := NewApp()
-	app.Run(":" + port)
-}
-
-type App struct {
-	r *gin.Engine
-}
-
-func NewApp() *App {
-	router := gin.New()
-
-	router.Use(gin.Logger())
-	router.Use(gin.Recovery())
-	router.Use(CORS())
-
-	config.Routes(router)
-
-	return &App{
-		r: router,
-	}
-}
-
-func (a *App) Router() *gin.Engine {
-	return a.r
-}
-
-func (a *App) Run(addr string) {
-	a.r.Run(addr)
-}
-
-// Default CORS middleware
-func CORS() gin.HandlerFunc {
-	return cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		MaxAge:           12 * time.Hour,
-		AllowCredentials: true,
-	})
+	app := NewApp("Airway", utils.GetEnvMust("AIRWAY_PORT"))
+	app.Run()
 }
