d9d95dee48e078a0533198d8a4d5e73a63e88d5b
David Zhang <daqing@mzevo.com>
2025-07-25 15:26:19 +0800

    Fix routes

--- config/routes.go
+++ config/routes.go
@@ -3,27 +3,22 @@ package config
 import (
 	"github.com/gin-gonic/gin"
 
-	"github.com/daqing/airway/app/api/asset_api"
-	"github.com/daqing/airway/app/api/page_api"
-	"github.com/daqing/airway/app/api/storage_api"
 	"github.com/daqing/airway/app/api/up_api"
 	"github.com/daqing/airway/app/websocket"
 )
 
 func Routes(r *gin.Engine) {
 	up_api.Routes(r)
-	asset_api.Routes(r)
 
 	websocketRoutes(r)
 	apiGroupRoutes(r)
 }
 
 func apiGroupRoutes(r *gin.Engine) {
-	v1 := r.Group("/api/v1")
-	{
-		page_api.Routes(v1)
-		storage_api.Routes(v1)
-	}
+	// v1 := r.Group("/api/v1")
+	// {
+	// 	storage_api.Routes(v1)
+	// }
 }
 
 func websocketRoutes(r *gin.Engine) {
